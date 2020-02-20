ace2f729ca86b0bc6c85b419ecb9b0a5bd40a1e8
Xudong QIN <steven_qxd@126.com>
2020-02-21 00:22:14 +0800

    Update test_all_QKD.py
    Add BB84 with Eve example.

--- test_all_QKD.py
+++ test_all_QKD.py
@@ -14,6 +14,12 @@ bisim.init_qLTS("examples/concreteBB84.txt", "parse_output/concreteBB84.gv",
           "examples/weak_concreteBB84-spec.txt", "parse_output/weak_concreteBB84-spec.gv")
 print("pLTS Generate in ", time.time() - qlts_timer)
 bisim.Weak_Bisimulation(0,0)
+# print("===================\nBB84 with an eavesdropper, need several hours")
+# qlts_timer = time.time()
+# bisim.init_qLTS("examples/concreteBB84-eavesdropper-m.txt", "parse_output/concreteBB84-eavesdropper-m.gv",
+#           "examples/weak_concreteBB84-eavesdropper-spec-m.txt", "parse_output/weak_concreteBB84-eavesdropper-spec-m.gv")
+# print("pLTS Generate in ", time.time() - qlts_timer)
+# bisim.Weak_Bisimulation(0,0)
 print("===================\nB92")
 qlts_timer = time.time()
 bisim.init_qLTS("examples/weak_concreteB92.txt", "parse_output/weak_concreteB92.gv",
